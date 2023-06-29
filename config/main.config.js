068af746beeed2bd08203abffaebfc31b29d9f24
Tushar Rai <tusharrai132@gmail.com>
2023-06-29 20:17:10 +0530

    code

--- config/main.config.js
+++ config/main.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  port: process.env.PORT || 3000,
+  port:  3000,
   mongoUrl:'mongodb+srv://verselon2000:W4iqNZvQcMrmNi31@cluster0.6j4igyn.mongodb.net/AirFlight?retryWrites=true&w=majority',
-  jwtSecret: process.env.JWTSECRET || "placeholder",
-  refreshSecret: process.env.REFRESHSECRET || "refreshholder",
-  tokenLife: process.env.TOKENLIFE || 300,
-  refreshLife: process.env.REFRESHLIFE || 86400,
+  jwtSecret:  "placeholder",
+  refreshSecret:"refreshholder",
+  tokenLife:  300,
+  refreshLife: 86400,
   permission: {
     NORMAL_USER: 1,
     ADMIN: 2047,
@@ -14,8 +14,8 @@ module.exports = {
     ADMIN: 1024,
   },
   // corsOrigin: "http://localhost:2020",
-  email: {
-    account: process.env.EMAIL_ACCOUNT,
-    pass: process.env.EMAIL_PASS,
-  },
+  // email: {
+  //   account: process.env.EMAIL_ACCOUNT,
+  //   pass: process.env.EMAIL_PASS,
+  // },
 };
